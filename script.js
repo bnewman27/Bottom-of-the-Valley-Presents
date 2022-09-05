59952603b092cdfb1957b17dcf80b2a20a63e806
Ben Newman <ben.newman52@ymail.com>
2022-09-05 11:46:21 -0700

    2nd edition

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 const targetElement = document.querySelector("#rainbow")
 new cursoreffects.rainbowCursor({element: targetElement});
 
-
+// nav start
+function menuOnClick() {
+    document.getElementById("menu-bar").classList.toggle("change");
+    document.getElementById("nav").classList.toggle("change");
+    document.getElementById("menu-bg").classList.toggle("change-bg");
+  }
+//   nav end
